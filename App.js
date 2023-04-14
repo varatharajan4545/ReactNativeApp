bad9611055592646c1643dfffdca407a9b4076ba
varatharajan4545 <svaratharajan4545@gmail.com>
2023-04-14 20:13:49 +0530

    Initial commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This Demo App</Text>
       <StatusBar style="auto" />
     </View>
   );
